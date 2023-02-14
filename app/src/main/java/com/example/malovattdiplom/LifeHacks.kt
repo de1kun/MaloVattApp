e3695aebc44a1d910666cfa3fc9f4c48f30e7dda
de1kun <aleksey3537@mail.ru>
2023-02-14 12:55:56 +0500

    MaloVatt0.8

--- app/src/main/java/com/example/malovattdiplom/LifeHacks.kt
+++ app/src/main/java/com/example/malovattdiplom/LifeHacks.kt
@@ -13,7 +13,7 @@ class LifeHacks : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_life_hacks)
-
+        val listView = findViewById<ListView>(R.id.listView)
         val lifeHacksList = ArrayList<String>()
         lifeHacksList.add("1. Гасите свет, переходя из комнаты в комнату. Установите тепловые датчики движения, которые будут выключать свет за вас.")
         lifeHacksList.add("2. Используйте местное освещение: подсветки, торшеры, бра. Например, чтобы каждый раз не включать основные источники света, в комнате лучше установить подсветку из светодиодной ленты.")
@@ -21,13 +21,10 @@ class LifeHacks : AppCompatActivity() {
         lifeHacksList.add("4. При ремонте учитывайте, что светлые стены и мебель будут отражать до 80% светового потока, а тёмные — лишь около 12%.")
         lifeHacksList.add("5. Замените лампочки накаливания на энергосберегающие и светодиодные. Замена только одной лампы позволит экономить около 1 000 рублей в год.")
         lifeHacksList.add("6. При отсутствии двухтарифного счётчика электроэнергии отключайте все неосновные электрические приборы на ночь, а зарядные устройства — после полной подпитки техники.")
-        lifeHacksList.add("7. Покупайте энергосберегающую бытовую технику!")
-        lifeHacksList.add("7. Покупайте энергосберегающую бытовую технику!")
-        lifeHacksList.add("7. Покупайте энергосберегающую бытовую технику!")
-        lifeHacksList.add("7. Покупайте энергосберегающую бытовую технику!")
-        lifeHacksList.add("7. Покупайте энергосберегающую бытовую технику!")
-        lifeHacksList.add("7. Покупайте энергосберегающую бытовую технику!")
+        lifeHacksList.add("В следующем обновлении !")
 
-        listView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, lifeHacksList)
+       val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, lifeHacksList)
+        listView.adapter = adapter
+
     }
 }
